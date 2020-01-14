0d19097e8996358844df3cceb11514c45d39f991
Alex Hong <thealexhong@gmail.com>
2020-01-14 14:40:58 -0500

    set up dev environment with postcss and husky

--- src/App.js
+++ src/App.js
@@ -1,40 +1,51 @@
-import React from 'react';
-import logo from './thealexhong.svg';
-import './App.css';
+import React from "react";
+import logo from "./assets/thealexhong.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>
-          Hi, I'm Alex Hong.
-        </h1>
+        <h1>Hi, I&apos;m Alex Hong.</h1>
         <h3>I love to create and bring products to market.</h3>
-        <p>I am the founder and CEO of <a
-          className="App-link"
-          href="https://robotplaytime.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >@robotplaytime</a>.
+        <p>
+          I am the founder and CEO of{" "}
+          <a
+            className="App-link"
+            href="https://robotplaytime.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            @robotplaytime
+          </a>
+          .
         </p>
 
-        <p>I am the co-founder of <a
-          className="App-link"
-          href="https://wonderleap.co"
-          target="_blank"
-          rel="noopener noreferrer"
-        >@wonderleap</a>.
+        <p>
+          I am the co-founder of{" "}
+          <a
+            className="App-link"
+            href="https://wonderleap.co"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            @wonderleap
+          </a>
+          .
         </p>
-        <p>I am obsess with complex body movements: <a
-          className="App-link"
-          href="https://www.instagram.com/thealexhong/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >@thealexhong</a>.
+        <p>
+          I am obsess with complex body movements:{" "}
+          <a
+            className="App-link"
+            href="https://www.instagram.com/thealexhong/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            @thealexhong
+          </a>
+          .
         </p>
-
-
       </header>
     </div>
   );
