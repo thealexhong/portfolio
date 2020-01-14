0db741b7585945641a1b65a1dc13d2153ef5708e
Alex Hong <thealexhong@gmail.com>
2020-01-14 16:12:15 -0500

    added styles

--- src/App.js
+++ src/App.js
@@ -3,15 +3,15 @@ import logo from "./assets/thealexhong.svg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="Home">
+      <header className="Home-header">
+        <img src={logo} className="Home-logo" alt="logo" />
         <h1>Hi, I&apos;m Alex Hong.</h1>
         <h3>I love to create and bring products to market.</h3>
         <p>
-          I am the founder and CEO of{" "}
+          I am the Founder and CEO of{" "}
           <a
-            className="App-link"
+            className="Home-link"
             href="https://robotplaytime.com"
             target="_blank"
             rel="noopener noreferrer"
@@ -22,9 +22,9 @@ function App() {
         </p>
 
         <p>
-          I am the co-founder of{" "}
+          I am the Co-founder of{" "}
           <a
-            className="App-link"
+            className="Home-link"
             href="https://wonderleap.co"
             target="_blank"
             rel="noopener noreferrer"
@@ -34,9 +34,9 @@ function App() {
           .
         </p>
         <p>
-          I am obsess with complex body movements:{" "}
+          I am obsessed with complex body movements:{" "}
           <a
-            className="App-link"
+            className="Home-link"
             href="https://www.instagram.com/thealexhong/"
             target="_blank"
             rel="noopener noreferrer"
@@ -45,6 +45,7 @@ function App() {
           </a>
           .
         </p>
+        <div className="Home-footer">Alex Hong &copy; 2020</div>
       </header>
     </div>
   );
